b9dec039e5ee239ab8efc91483f53ffd4703d6a3
root <root@debian-s-1vcpu-1gb-ned1-01.localdomain>
2020-08-29 09:47:46 +0000

    Coment code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// Load express package dependency 
 const express = require('express');
+// Create a new express app
 const app = express();
+// Create an http-proxy-middleware while loading the corresponding package
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use('/', createProxyMiddleware({ target: 'https://aristidebouix.cloud', cha$
+// The express app input a target website
+app.use('/', createProxyMiddleware({ target: 'https://aristidebouix.cloud', changeOrigin: true }));
+// The target website can be accessed on localhost port 3000
 app.listen(3000);
 
